253ca3c4e5adec13781f1ef8fc20f22628cbff7c
Romain Sorrenti <romainsorrnti@gmail.com>
2019-10-24 11:53:30 +0200

    Add UserMarker

---
+++ src/components/UserPosMarker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Icon } from 'antd';
+
+const UserPosMarker = (props) => {
+
+  return (
+    <div>
+     
+    <span  className="brand-red"></span>
+    <Icon type="home" style={{ fontSize: '20px' }}  theme="twoTone" />
+
+  </div>
+  );
+};
+
+export default UserPosMarker;
