8e8dc0a9fcd3163b03c1b74c50d95448ad503df2
Romain Sorrenti <romainsorrnti@gmail.com>
2019-12-04 13:43:19 +0100

    Adding visibility handler

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon } from 'antd';
 
 const MapMarker = (props) => {
 
+  const [visibility, setVisibility] = useState('hide');
+  const { name } = props
+
+  const visibilityHandler = () => {
+    if (visibility === 'hide') {
+      setVisibility('')
+    } else {
+      setVisibility('hide')
+    }
+  }
+
   return (
     <div>
-      <span id={props.id} style={{ fontSize: '18px' }}  className="brand-red"></span>
-      <Icon className="font-1-5" type="environment" style={{ fontSize: '20px' }}   theme="twoTone" twoToneColor="#fd0000" />
+      <span 
+        id={props.id}
+        style={{ fontSize: '16px', color: 'black' }}  
+        className="brand-red">
+          <Icon className="font-1-5"
+            onClick={visibilityHandler}
+            type="environment" style={{ fontSize: '20px' }}   theme="twoTone" twoToneColor="#fd0000" 
+          />
+        <p className={visibility} style={{backgroundColor: '#ffffff', opacity: '0.8'}}> {name}</p>
+      </span>
     </div>
   );
 };
