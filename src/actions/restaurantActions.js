d7c326dd6008f04c9e526a579ed1aa9ba0a3cf79
Romain Sorrenti <romainsorrnti@gmail.com>
2019-12-05 08:55:54 +0100

    comment

--- src/actions/restaurantActions.js
+++ src/actions/restaurantActions.js
@@ -4,7 +4,8 @@ import {
 
 // Get Restaurants from serveur
 // function returning takes a dispatch which allow us to dispatching to the reducer at any time 
-// meaning we can make the request to our backend and then wait get the response an then dispatch to the reducer
+// Je veux pouvoir attendre la réponse pour envoyer mon distach ensuite
+
 export const getRestaurants = () => async dispatch => {
         const res = await fetch('http://localhost:5000/restaurants');
         const data = await res.json(); // stocker le corp de la requête en JSON
