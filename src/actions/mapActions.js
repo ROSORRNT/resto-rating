630b67adab63294a3b72a4dab795e5219ba56ed3
Romain Sorrenti <romainsorrnti@gmail.com>
2019-12-06 10:19:07 +0100

    Adding error option + dispatch props permision

--- src/actions/mapActions.js
+++ src/actions/mapActions.js
@@ -8,16 +8,32 @@ elle prend un dispatch qui nous permette d’envoyer au reducer à tout moment (
 Ainsi nous pouvons effectuer notre et attendre la réponse pour l'envoyer au reducer
 */
 export const getUserPosition = () => (dispatch) => {
+
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition((position) => {
-            const data = { lat: position.coords.latitude, lng: position.coords.longitude }
+            let data = { lat: position.coords.latitude, lng: position.coords.longitude, permision: true }
             dispatch({
                 type: SET_USER_POSITION,
                 payload: data
             })    
+        }, (error) => {
+            // console.log(error.code) // if error.code === 1 //=> Message : géolocalisation refusée
+            let data = {
+                lat: 43.6535296,
+                lng: 7.1458816, 
+                permision: false
+            }
+            dispatch({
+                type: SET_USER_POSITION,
+                payload: data
+            })
         });
     } else {
-        const data = { lat: null, lng: null }
+        let data = {
+            lat: 43.6535296,
+            lng: 7.1458816, 
+            permision: false
+        }
         dispatch({
             type: SET_USER_POSITION,
             payload: data
